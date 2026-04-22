6b3c822ad0573732c54e97bad02f03848b4dbd51
Rut Vaghani <rutvaghani@Ruts-MacBook-Air.local>
2026-04-22 10:17:35 +0530

    changes in main.py for backend deployment

--- backend/main.py
+++ backend/main.py
@@ -257,5 +257,7 @@ def _mock_analyze(s):
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
